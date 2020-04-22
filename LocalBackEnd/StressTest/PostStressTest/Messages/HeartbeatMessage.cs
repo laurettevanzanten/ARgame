f29ad65903373055dea778c9e56a6517bdeb693a
pointlesspun <anotherpointlesspun@gmail.com>
2020-04-22 17:49:35 +0200

    Added all stress test messages

---
+++ LocalBackEnd/StressTest/PostStressTest/Messages/HeartbeatMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostStressTest.Messages
+{
+    public class HeartbeatMessage
+    {
+        public string token { get; set; }
+        public int timeStamp { get; set; }
+    }
+}
